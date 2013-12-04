84f30340f431e5472a7e5336b85cd0d1558dea8a
mattots <github@mattots.com>
2013-12-04 12:15:02 +0000

    Ignore empty style blocks/stylesheets. Improve commenting.

--- polycalc.js
+++ polycalc.js
@@ -1,11 +1,17 @@
 if ( ! window.jQuery || ! CSSParser) {
+
+	// Check dependencies
 	alert("PolyCalc requires jQuery and JSCSSP to function. Disabling.");
+
 } else {
-	if(typeof String.prototype.startsWith != 'function') {
-		String.prototype.startsWith = function(str){
-			return this.indexOf(str) == 0;
-		};
+
+	// Define custom 'startsWith' function
+	if (typeof String.prototype.startsWith != 'function') {
+		String.prototype.startsWith = function (str){
+    		return this.slice(0, str.length) == str;
+  		};
 	}
+
 	
 	$(document).ready(function() {
 
@@ -13,20 +19,30 @@ if ( ! window.jQuery || ! CSSParser) {
 
 			this.abort = false;
 			
+
 			this.initiate = function() {
 
 				var parser = new CSSParser();
-				var styleSheets = $("style");
+
+
+				// -------------------------------------------------------------
+				// Call the relevant function depending on location of styles
 				
+				// In-page <style> blocks...
+				var styleSheets = $("style");
 				styleSheets.each(function() {
-					parseStyleSheet(parser, $(this).html());
+					if ($(this).html() !== '') { // ignore empty style blocks
+						parseStyleSheet(parser, $(this).html());
+					}
 				});
 				
-				// Stylesheets...
+				// Linked stylesheets...
 				styleSheets = $("link[rel='stylesheet']");
 				styleSheets.each(function() {
 					$.get($(this).attr("href"), function(data){
-						parseStyleSheet(parser, data);
+						if (data !== '') { // ignore empty stylsheets
+							parseStyleSheet(parser, data);
+						}
 					});
 				});
 				
@@ -41,10 +57,16 @@ if ( ! window.jQuery || ! CSSParser) {
 					}
 				});
 			}
+
+
+
+			// -------------------------------------------------------------------------
+			// Depending on location of styles, either loop over selectors or properties and call relevant function to process further
 			
+			// For in-page <style> blocks and linked stylesheets...
 			var parseStyleSheet = function(parser, source) {
 				var styleSheet = parser.parse(source, false, false);
-				
+
 				var selectors = styleSheet.cssRules;
 				for(var i = 0; i < selectors.length; ++i) {
 					var selector = selectors[i];
@@ -53,78 +75,107 @@ if ( ! window.jQuery || ! CSSParser) {
 				}
 			}
 			
+			// For inline styles...
 			var parseInline = function(parser, element) {
 				var source = "* { " + element.attr("style") + " }";
 				var style = parser.parse(source, false, false);
 				
-				var properties = style.cssRules[0].declarations;
-				
+				// get properties
+				var properties = style.cssRules[0].declarations;				
 				for(var i = 0; i < properties.length; ++i) {
 					var property = properties[i];
 					
-					parseProperty(element, property, true);
+					parseProperty(element, property, true); // 3rd arg is 'elementKnown'. Because these are inline styles we know what the element is as the style is attached directly to an HTML element)
 				}
 			}
+
+
 			
+			// For in-page <style> blocks and linked stylesheets, parse selector first, then call parseProperty()
+			// selector = e.g. 'width'
 			var parseSelector = function(selector) {
-				var properties = selector.declarations;
-				
+				var properties = selector.declarations; // get properties
+
 				if (properties !== undefined) {
 					for(var i = 0; i < properties.length; ++i) {
 						var property = properties[i];
 						
-						parseProperty(selector, property, false);
+						parseProperty(selector, property, false); // 3rd arg is 'elementKnown'. Because these are stylesheet styles we don't definitely know what the element is as the style could be defined on a class or id rather than an HTML element.)
 					}
 				}
 			}
-			
+
+
+
+			// Parse CSS property
+			// property = e.g. '100px'
 			var parseProperty = function(selector, property, elementKnown) {
+				
 				var values = property.values;
+
+				// Loop over each value in the property, in case it's a shorthand style rule containing multiple values.
 				for(var i = 0; i < values.length; ++i) {
 					var value = values[i];
 
-					if( ! elementKnown)
-						var selectorValue = selector.selectorText();
+					if( ! elementKnown) {
+						var selectorValue = selector.selectorText(); // If element not known, retrieve the HTML element this selector applies to
+					}
 						
 					var propertyValue = property.property;
 					var valueValue = value.value;
 
+					// Does the value contain 'calc(' or '-calc('? (Takes into account browser prefixes). If so, process it.
 					if ((valueValue.indexOf("calc(") !== -1) && (valueValue.indexOf("-calc(") === -1)) {
+
 						if (elementKnown) {
 							elements = selector;
 						} else {
 							elements = $(selectorValue);
 						}
 						
+						// Calculate calc() and return as a fixed px size
 						var update = function() {
+
+							// Loop over each element in the document that this style applies to
 							elements.each(function() {
 								var newValue = parseExpression(propertyValue, valueValue, $(this)) + "px";
+								// apply the caclulcated value to the relevant element in the DOM
 								$(this).css(propertyValue, newValue);
 							});
 						}
 
-						// re-calculate calc() on load and window resize events
+						// Call update() on load and window resize events
 						$(window).resize(update);
 						update();
 					}
 				}
 			}
 			
+			// Parse the calc() expression
 			var parseExpression = function(propertyValue, expression, element) {
+				// propertyValue : e.g. 'width'
+				// expression : e.g. 'calc(50% + 20px)'
+				// element : the actual DOM object the style relates to
 
 				var newExpression = "";
 
+				// retrieve the stuff that needs calculating from the calc(...) expression
 				regex = expression.match(/^calc\((.+)\)$/);
 				if (regex !== null) {
 					expression = expression.match(/^calc\((.+)\)$/)[1];
 				}
 				
 				var value = -1;
+				// Loop over each character inside calc()
 				for(var i = 0; i < expression.length; ++i) {
+					
 					var substr = expression.substring(i);
 					
+					// Numbers
 					var regex = substr.match(/^[\d.]+/);
 					if(regex !== null) {
+
+						// Convert number string into a floating point number
 						value = parseFloat(regex[0], 10);
 						
 						i += regex[0].length - 1;
@@ -132,9 +183,13 @@ if ( ! window.jQuery || ! CSSParser) {
 						continue;
 					}
 					
+					// Units ( px, %, em, rem, in, pt, pc, mm, cm )
 					regex = substr.match(/^([A-Za-z]+|%)/);
 					if (regex !== null) {
+
+						// Convert to a corresponding px value
 						value = convertUnit(regex[1], "px", value, propertyValue, element);
+						
 						if (value !== -1)
 							newExpression += value;
 							
@@ -144,17 +199,19 @@ if ( ! window.jQuery || ! CSSParser) {
 						continue;
 					}
 					
+					// Math operators and symbols ( +, -, *, /, (, ) )
 					var char = expression.charAt(i);
-					
 					if(char === '+' || char === '-' || char === '*' || char === '/' || char === '(' || char === ')') {
 						newExpression += char;
 						value = -1;
 					}
 				}
 
+				// Do the maths and return the px value!
 				return eval(newExpression);
 			}
 			
+			// Convert any kind of CSS unit to it's px value
 			var convertUnit = function(from, to, value, propertyValue, element) {
 				switch(to) {
 					case "px": {
